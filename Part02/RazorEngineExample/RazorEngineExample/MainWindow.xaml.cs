197f37a965a0e0cb86fd7af3f1a2aa8ab813f14e
Rob Feng <rob.feng@software.dell.com>
2016-11-26 18:06:02 +0800

    use my RazorEngineService

--- Part02/RazorEngineExample/RazorEngineExample/MainWindow.xaml.cs
+++ Part02/RazorEngineExample/RazorEngineExample/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using RazorEngine;
+using RazorEngine.Configuration;
 using RazorEngine.Templating;
 using System;
 using System.Collections.Generic;
@@ -17,20 +18,6 @@ using System.Windows.Shapes;
 
 namespace RazorEngineExample
 {
-
-    public class SubModel
-    {
-        public string SubModelProperty { get; set; }
-    }
-
-    public class MyModel
-    {
-        public string ModelProperty { get; set; }
-        public SubModel SubModel { get; set; }
-
-        public string SubModelTemplateName { get; set; }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -45,23 +32,35 @@ namespace RazorEngineExample
 
         void LoadHtml()
         {
+
+
+            var config = new TemplateServiceConfiguration();
+            // You can use the @inherits directive instead (this is the fallback if no @inherits is found).
+            config.BaseTemplateType = typeof(TemplateBase<>);
+            // Custom template manager http://antaris.github.io/RazorEngine/TemplateManager.html
+            //config.TemplateManager = new MyTemplateManager();
+
+            using (var service = RazorEngineService.Create(config))
+            {
+                GenerateHtml(service);
+            }
+        }
+
+        private void GenerateHtml(IRazorEngineService service)
+        {
             var SampleTemplate = File.ReadAllText("SampleTemplate.cshtml");
             var SubSample = File.ReadAllText("SubSample.cshtml");
 
-            var service = Engine.Razor;
-
-            // In this example I'm using the default configuration, but you should choose a different template manager: http://antaris.github.io/RazorEngine/TemplateManager.html
-            service.AddTemplate("part", SubSample);
-            //service.AddTemplate("part", "my template + @Model");
+            //service.AddTemplate("part", SubSample);
 
             // If you leave the second and third parameters out the current model will be used.
             // If you leave the third we assume the template can be used for multiple types and use "dynamic".
             // If the second parameter is null (which is default) the third parameter is ignored.
             // To workaround in the case you want to specify type "dynamic" without specifying a model use Include("p", new object(), null)
-            service.AddTemplate("template", SampleTemplate);
+            //service.AddTemplate("template", SampleTemplate);
 
-            service.Compile("template", typeof(MyModel));
-            service.Compile("part", typeof(SubModel));
+            service.Compile(SampleTemplate, "template", typeof(MyModel));
+            service.Compile(SubSample, "part", typeof(SubModel));
 
             var result = service.Run("template", typeof(MyModel), new MyModel
             {
@@ -70,12 +69,11 @@ namespace RazorEngineExample
                 SubModel = new SubModel { SubModelProperty = "submodel2" }
             });
 
-
             Console.WriteLine("Result is: {0}", result);
-
 
             DisplayHtml(result);
         }
+
 
         private void DisplayHtml(string aHtml)
         {
---
+++ Part02/RazorEngineExample/RazorEngineExample/MyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorEngineExample
+{
+    public class MyModel
+    {
+        public string ModelProperty { get; set; }
+
+        public SubModel SubModel { get; set; }
+
+        public string SubModelTemplateName { get; set; }
+    }
+}
